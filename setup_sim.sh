33084155252eebba2a1f1967fe2bbac7a7dded9c
Christopher Jury <chrisj@systemizeconsulting.com.au>
2020-01-14 09:18:37 +1100

    Modifications for simulating different iris models, was not working properly with previous posix_sitl.launch
    	* Tools/sitl_gazebo - committed in changes from the stmz fork
    
    	* launch/posix_sitl.launch - added model parameter, usage (vehicle:=iris, model:=iris_dual_camera)
    
    	* setup_sim.sh - script for setting all required simulation env vars, run before a simulation

---
+++ setup_sim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Setup environment to make PX4 visible to Gazebo.
+#
+# Note, this is not necessary if using a ROS catkin workspace with the px4
+# package as the paths are exported.
+#
+# License: according to LICENSE.md in the root directory of the PX4 Firmware repository
+
+DONT_RUN=1 make px4_sitl_default gazebo
+source ~/catkin_ws/devel/setup.bash    # (optional)
+source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
